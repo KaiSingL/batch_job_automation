c9fe4749b74bb54ab93ad2e0c8b5cba09d6ba83a
Sing L <laut9810@gmail.com>
2020-10-02 23:40:40 +0800

    Create ~.bat

---
+++ ~.bat
@@ -0,0 +1,6 @@
+@echo off
+rem This is a shortcut for returning to user's home directory
+rem Following the linus convention
+rem ----------------------------------------------------------
+
+cd C:\Users\yourname
